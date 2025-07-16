12b7998d4d214f43cbd8ac2ed6c5db93d6c5e42b
Ahmed Farhat <125553908+ahmedfarhat25@users.noreply.github.com>
2025-07-16 17:15:13 +0300

    update in structure and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,28 @@
-import React from 'react';
 import QRAdSection from './components/QRAdSection';
-import WhyChooseSection from './components/WhyChooseSection';
-import FeaturesCircle from './components/FeaturesCircle';
 import FeatureCards from './components/FeatureCards';
-import PackagesSection from './components/PackagesSection';
 import PartnersSection from './components/PartnersSection';
 import ContactForm from './components/ContactForm';
-import FeaturesDetailsSection from './components/FeaturesDetailsSection';
 import FetureQrmiddleSection from './components/FetureQrmiddleSection';
 import IcodeFeture from './components/IcodeFeture';
 import NavBar from './components/NavBar';
 import PhoneForm from './components/PhoneForm';
 import PhoneMockup from './components/test';
-import OrangeHeroFormSection from './components/OrangeHeroFormSection';
+
 import SubscriptionForm from './components/SubscriptionForm';
+
 function App() {
   return (
-      <div className="App">
+    <div className="App min-h-screen text-gray-800">
       <NavBar />
-
-      <div id="qr-ad" className="section"><QRAdSection /></div>
-      <div id="qr-middle" className="section"><FetureQrmiddleSection /></div>
-      <div id="test" className ="section"><PhoneMockup /></div>
-      <div id="icode-feature" className="section"><IcodeFeture /></div>
-      <div id="partners" className="section"><PartnersSection /></div>
-       <div id="phone-form" className="section"> <PhoneForm /> </div>
-      <div id="feature-cards" className="section"><FeatureCards /></div>
-      <div id="features-details" className="section FeaturesDetailsSection-section"><FeaturesDetailsSection /></div>
-      <div id="contact" className="section"><ContactForm /></div>
-      <div id="subscription-form" className="section"><SubscriptionForm /></div>
+      <section id="qr-ad" className=""><QRAdSection /></section>
+      <section id="qr-middle" className=""><FetureQrmiddleSection /></section>
+      <section id="test" className=""><PhoneMockup /></section>
+      <section id="icode-feature" className=""><IcodeFeture /></section>
+      <section id="partners" className=""><PartnersSection /></section>
+      <section id="phone-form" className=""><PhoneForm /></section>
+      <section id="feature-cards" className=""><FeatureCards /></section>
+      <section id="contact" className=""><ContactForm /></section>
+      <section id="subscription-form" className=""><SubscriptionForm /></section>
     </div>
   );
 }
