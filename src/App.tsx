182c0095396cce2a247a85b3cb4a90294289a634
Ahmed Farhat <125553908+ahmedfarhat25@users.noreply.github.com>
2025-07-16 23:41:07 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import QRAdSection from './components/QRAdSection';
+import QRAdSectionion from './components/QRAdSection';
 import FeatureCards from './components/FeatureCards';
 import PartnersSection from './components/PartnersSection';
 import ContactForm from './components/ContactForm';
@@ -14,6 +14,7 @@ function App() {
   return (
     <div className="App min-h-screen text-gray-800">
       <NavBar />
+      <section id="qr home" className=""> <QRAdSectionion/></section>
       <section id="qr-middle" className=""><FetureQrmiddleSection /></section>
       <section id="test" className=""><PhoneMockup /></section>
       <section id="icode-feature" className=""><IcodeFeture /></section>
