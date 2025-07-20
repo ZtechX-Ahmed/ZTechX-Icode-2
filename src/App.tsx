d1aa8c2251de90e52b60927980902fabff443203
ZtechX-Ahmed <elharrefgamingg3a@gmail.com>
2025-07-20 23:11:55 +0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import QRAdSectionion from './components/QRAdSection';
+import QRAdSection from './components/QRAdSection';
 import FeatureCards from './components/FeatureCards';
 import PartnersSection from './components/PartnersSection';
 import ContactForm from './components/ContactForm';
@@ -6,7 +6,9 @@ import FetureQrmiddleSection from './components/FetureQrmiddleSection';
 import IcodeFeture from './components/IcodeFeture';
 import NavBar from './components/NavBar';
 import PhoneForm from './components/PhoneForm';
-import PhoneMockup from './components/test';
+import SecondPage from './components/test';
+import ThirdPage from './components/thirdpage'; 
+import FourthPage from './components/fourth page';
 
 import SubscriptionForm from './components/SubscriptionForm';
 
@@ -14,8 +16,9 @@ function App() {
   return (
     <div className="App min-h-screen text-gray-800">
       <NavBar />
-      <section id="qr-ad" className=""> <QRAdSectionion/></section>
-      <section id="qr-middle" className=""><FetureQrmiddleSection /></section>
+      <section id="qr-ad" className=""><QRAdSection /></section>
+      <section id="phone-mockup" className=""><SecondPage /></section>
+      <section id ="third-page" className=""><ThirdPage /></section>
       <section id="icode-feature" className=""><IcodeFeture /></section>
       <section id="partners" className=""><PartnersSection /></section>
       <section id="phone-form" className=""><PhoneForm /></section>
